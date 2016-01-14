d5821f3aa6b2af901c55bc9879f3019453288496
fisherj18 <jamiefisher747@gmail.com>
2016-01-13 16:41:28 -0800

    Fixed prime number testing

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -14,13 +14,15 @@ public class PrimeTester {
      * @return true iff n is prime
      */
     public static boolean isPrime(long n) {
-        int count = 0;
-        for(int i=2;i<n;i++) {
-            if ((n/i)%==0){
-                count++;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
             }
         }
-        if(count==1) return true;
-        return false;
+        if(n==0 || n==1){
+            return false;
+        }
+        return true;
+
     }
 }
